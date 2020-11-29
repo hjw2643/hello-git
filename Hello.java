ada9476e58db7375865fd6acc05ec1d08677aeb2
hjw <1694082@hansung.ac.kr>
2020-11-29 02:21:39 +0000

    Sencond commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 // Hello.java
-public class Hello
+public class Hello {
   public static void main(String[] args) {
 	  system.out.println("Hellom world from GIT!");
+	  System.out.println("Changes after First commit!");
   }
+}
